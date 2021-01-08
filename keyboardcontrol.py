c31fcf538068a7840394bf1963b2dc0b0f9d4901
Saifuddin Mohammad <38083799+yaxis1@users.noreply.github.com>
2021-01-08 01:25:21 +0100

    sending keyboard input to drone

--- keyboardcontrol.py
+++ keyboardcontrol.py
@@ -10,5 +10,28 @@ print(drone.get_battery())
 
 drone.takeoff() 
 
+def getkeyinput():
+    lr,fb,ud,yv = 0,0,0,0 
+    if kp.getkey("LEFT"): lr = -50 
+    elif kp.getkey("RIGHT"): lr = 50
+
+    if kp.getkey("UP"): fb = 50
+    elif kp.getkey("DOWN"): fb = -50
+
+    if kp.getkey("w"): ud = 50
+    elif kp.getkey("s"): ud = -50
+
+    if kp.getkey("a"): yv = 50
+    elif kp.getkey("d"): yv = -50
+
+    if kp.getkey('e'): drone.takeoff()
+    if kp.getkey('q'): drone.land() #INCASE DRONE GOES MAD
+
+    return [lr,fb,ud,yv]
+    
 while True: 
-    drone.send_rc_control(0,0,0,0)
+    vals = getkeyinput()
+    #sending keyinputs to drone
+    drone.send_rc_control(vals[0],vals[1],vals[2],vals[3])
+    
+    sleep(0.05) #INCASE THE DRONE CREATES HAVOC
